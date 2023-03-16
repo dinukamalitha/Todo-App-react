ee05c68008cff75399fa9a95df2ed0c03df9b9f4
dinukamalitha <dmcoder01@gmail.com>
2023-03-16 23:52:30 +0530

    States Created

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,25 @@
 import React from "react";
 
-const Form = () => {
+const Form = ({inputText, setInputText, todos, setTodos}) => {
+    //JS Functions
+    const inputTextHandler = (e) => {
+        console.log(e.target.value);
+        setInputText(e.target.value);
+    };
+
+    const submitTodoHandler = (e) => {
+        //console.log("hey");
+        e.preventDefault();
+        setTodos([
+            ...todos,
+            {text:inputText, completed:false, id:Math.random()*1000}
+        ])
+    };
+
     return(
         <form>
-            <input type="text" className="todo-input"/>
-            <button className="todo-button" type="submit">
+            <input onChange={inputTextHandler} type="text" className="todo-input"/>
+            <button onClick={submitTodoHandler} className="todo-button" type="submit">
                 <i className="fas fa-plus-square"></i>
             </button>
             <div className="select">
