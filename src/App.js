b993df7d100513067ab518a683ff3db384c3df65
dinukamalitha <dmcoder01@gmail.com>
2023-03-17 14:21:18 +0530

    Input Handling Created

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
-  const [inputText, setInputText] = ("");
-  const [todos, setTodos] = ([]);
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
           <h1>My Todo List</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
+      <Form
+          inputText={inputText}
+          todos={todos}
+          setTodos={setTodos}
+          setInputText={setInputText}
+      />
       <TodoList/>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -12,13 +12,19 @@ const Form = ({inputText, setInputText, todos, setTodos}) => {
         e.preventDefault();
         setTodos([
             ...todos,
-            {text:inputText, completed:false, id:Math.random()*1000}
-        ])
+            {text: inputText, completed: false, id: Math.random()*1000}
+        ]);
+        setInputText("");
     };
 
     return(
         <form>
-            <input onChange={inputTextHandler} type="text" className="todo-input"/>
+            <input
+                value={inputText}
+                onChange={inputTextHandler}
+                type="text"
+                className="todo-input"
+            />
             <button onClick={submitTodoHandler} className="todo-button" type="submit">
                 <i className="fas fa-plus-square"></i>
             </button>
